b86ce896d7da8115394363081ffa70069fe931b9
Ibrahim  Akar <ibsakar@gmail.com>
2019-05-14 16:09:54 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,16 +1,28 @@
 class School
-  
   def initialize(name)
-    @name = name 
+    @name = name
     @roster = {}
-  end 
-  
-  def roster 
-    @roster 
-  end 
+  end
 
-def add_student(name, grade)
-  roster << name 
-  roster << grade 
-end 
+   def roster
+    @roster
+  end
+
+   def add_student(name, grade)
+    if @roster[grade] != nil
+      @roster[grade] << name
+    else
+      @roster[grade] = [name]
+    end
+  end
+
+   def grade(grade)
+    @roster[grade]
+  end
+
+   def sort
+    @roster.each do |key, value|
+      value.sort!
+    end
+  end
 end 
